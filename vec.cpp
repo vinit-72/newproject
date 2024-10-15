7adf16dc84c6f8e1543afad755d85601b62a3ae2
Sai Udheep <2023ucp1971@mnit.ac.in>
2024-10-15 14:47:22 +0530

    change

---
+++ vec.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> v1 = {1,5,7};
+    vector<int> v2 = {3,8,2};
+    int result = 0;
+
+    for (int i = 0; i < v1.size(); i++)
+    {
+        result+=v1[i]*v2[i];
+    }
+    cout<<"answer:"<<result<<endl;
+ 	cout<<"hi"<<endl;   
+}
